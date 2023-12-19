d3acaa53c52e45343932631ba474b4dff9b90e56
JonesqPacMan <adiardiansyah.official@gmail.com>
2023-12-19 09:07:08 +0700

    Initial Release

---
+++ README.md
@@ -0,0 +1,65 @@
+### TWRP device tree for Xiaomi 12T (plato)
+
+=========================================
+
+The Xiaomi 12T (codenamed _"plato"_) is a high-end, mid-range smartphone from Xiaomi.
+
+It was released in October 2022.
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Octa-core CPU with 4x Arm Cortex-A78 up to 2.85GHz
+Chipset | Mediatek Dimensity 8100
+GPU     | Mali-G610 MC6
+Memory  | 8 GB RAM (LPDDR5 6400Mbps)
+Shipped Android Version | 12
+Storage | 128/256 GB (UFS 3.1)
+Battery | Li-Po 5000 mAh, non-removable
+Display | 1220 x 2712 pixels, 6.67 inches, 60/120hz
+
+![Xiaomi 12T](https://i02.appmifile.com/898_operator_sg/26/08/2022/fc94660da1d6dd006f7589327bb72813.png)
+
+## Features
+
+Works:
+
+- [X] ADB
+- [X] Decryption (Android 14)
+- [X] Display
+- [X] Fasbootd
+- [X] Flashing
+- [X] MTP
+- [X] Sideload
+- [X] USB OTG
+- [X] Vibrator
+
+## Compile
+
+First checkout minimal twrp with aosp tree:
+
+```
+repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1
+repo sync -j$(nproc --all)
+```
+
+Then add these projects to .repo/manifest.xml:
+
+```xml
+<project path="device/xiaomi/plato" name="JonesqPacMan/android_device_xiaomi_plato_TWRP" remote="github" revision="TWRP-12.1_A14-HyperOS" />
+```
+
+Finally execute these:
+
+```
+source build/envsetup.sh
+repopick <needed patch>
+lunch twrp_plato-eng
+mka vendorbootimage -j$(nproc --all)
+```
+## To use it:
+
+```
+fastboot flash vendor_boot twrp-3.7.0-12.1_A14-HyperOS-vendor_boot-plato.img
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,93 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2022 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+FOX_MANIFEST_ROOT=$(gettop)
+if [ -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox_defaults.go -a -f $FOX_MANIFEST_ROOT/bootable/recovery/orangefox.mk ]; then
+    if [ -z "$TW_DEFAULT_LANGUAGE" ]; then
+        unset TW_DEFAULT_LANGUAGE
+        export TW_DEFAULT_LANGUAGE="id_ID"
+    fi
+
+    # Build
+    export LC_ALL="C"
+    export FOX_VERSION=$(date +%y.%m.%d)
+    export ALLOW_MISSING_DEPENDENCIES=true
+    export TARGET_DEVICE_ALT="plato"
+    export FOX_VIRTUAL_AB_DEVICE=1
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    export OF_MAINTAINER="JonesqPacMan™"
+
+    # Magiskboot
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+    export OF_PATCH_VBMETA_FLAG=1
+
+    # Magisk
+    export FOX_USE_SPECIFIC_MAGISK_ZIP="$FOX_MANIFEST_ROOT/device/xiaomi/plato/Magisk/Magisk-v25.2.zip"
+
+    # Binaries
+    export FOX_USE_BASH_SHELL=1
+    export FOX_ASH_IS_BASH=1
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_GREP_BINARY=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_ZIP_BINARY=1
+    export FOX_USE_SED_BINARY=1
+    export FOX_REPLACE_TOOLBOX_GETPROP=1
+    export FOX_USE_XZ_UTILS=1
+    export FOX_REPLACE_BUSYBOX_PS=1
+    export OF_ENABLE_LPTOOLS=1
+
+    # Decryption
+    export OF_FBE_METADATA_MOUNT_IGNORE=1
+    export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+    export OF_NO_RELOAD_AFTER_DECRYPTION=1
+
+    # Screen
+    export OF_SCREEN_H=2400
+    export OF_STATUS_H=50
+    export OF_STATUS_INDENT_LEFT=85
+    export OF_STATUS_INDENT_RIGHT=85
+    export OF_HIDE_NOTCH=1
+    export OF_CLOCK_POS=1
+    export OF_ALLOW_DISABLE_NAVBAR=0
+
+    # Backup
+    export OF_QUICK_BACKUP_LIST="/boot;/vendor_boot;/data;"
+
+    # OTA & MIUI
+    export OF_NO_MIUI_PATCH_WARNING=1
+    export OF_PATCH_AVB20=1
+    # export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+
+    # Others
+    export OF_USE_GREEN_LED=0
+    export OF_FLASHLIGHT_ENABLE=0
+    
+
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+        export | grep "FOX" >>$FOX_BUILD_LOG_FILE
+        export | grep "OF_" >>$FOX_BUILD_LOG_FILE
+        export | grep "TARGET_" >>$FOX_BUILD_LOG_FILE
+        export | grep "TW_" >>$FOX_BUILD_LOG_FILE
+    fi
+fi
